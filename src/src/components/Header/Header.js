83d801456fbe7ebcb53c9be175ebe84ccdde4287
flintbean <ajmerando1@yahoo.com>
2018-12-21 16:54:55 -0800

    added header

---
+++ src/src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Header.css";
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="header-ctn">
+        <Link to="/" style={{textDecoration: 'none'}}>
+          <div className="nav-link">Home</div>
+        </Link>
+        <div className="nav-link">About</div>
+        <div className="nav-link">Projects</div>
+        <a className="nav-link" href="https://github.com/flintbean" style={{textDecoration: 'none'}}>
+          GitHub
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Header;

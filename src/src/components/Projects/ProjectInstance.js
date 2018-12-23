f3a2c3c70da5f68ce917149199f6cf31e1102234
flintbean <ajmerando1@yahoo.com>
2018-12-22 17:09:07 -0800

    removed console log

--- src/src/components/Projects/ProjectInstance.js
+++ src/src/components/Projects/ProjectInstance.js
@@ -1,6 +1,5 @@
 import React from "react";
 const ProjectInstance = props => {
-  console.log(props)
   return (
     <div>
       <a href={props.project.link} style={{ textDecoration: "none", color: "#FF0000" }}>

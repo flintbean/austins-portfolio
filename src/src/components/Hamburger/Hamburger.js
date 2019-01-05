ce71f333d18c5de252ad9715e779b351c5911b97
Flintbean <ajmerando1@yahoo.com>
2019-01-05 14:56:50 -0800

    moved hamburger css to appropriate directory

--- src/src/components/Hamburger/Hamburger.js
+++ src/src/components/Hamburger/Hamburger.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '../../App.css'
+import './Hamburger.css'
 
 const Hamburger = props => {
   return (

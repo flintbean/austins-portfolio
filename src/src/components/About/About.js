1b68880e6056684116a534e72bf99bf51670a8cc
Flintbean <ajmerando1@yahoo.com>
2019-01-05 19:44:46 -0800

    added mobile responsiveness for all components

--- src/src/components/About/About.js
+++ src/src/components/About/About.js
@@ -8,9 +8,12 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-ctn">
-      <h3>About Me</h3>
-        This is where I am going to put a picture of myself and do a little
-        explaining on what I like to do and my areas of expertise. The languages that I am good with and maybe the schools I went to?
+        <h3>About Me</h3>
+        <p className="about-text">
+          This is where I am going to put a picture of myself and do a little
+          explaining on what I like to do and my areas of expertise. The
+          languages that I am good with and maybe the schools I went to?
+        </p>
       </div>
     );
   }

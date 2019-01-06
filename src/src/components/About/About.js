a0f444614f387de44d66833dad79a8f37571ce97
Flintbean <ajmerando1@yahoo.com>
2019-01-05 20:07:12 -0800

    fixed responsivness to adapt to at higher pixel size

--- src/src/components/About/About.js
+++ src/src/components/About/About.js
@@ -8,7 +8,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-ctn">
-        <h3>About Me</h3>
+        <h3 className="about-header">About Me</h3>
         <p className="about-text">
           This is where I am going to put a picture of myself and do a little
           explaining on what I like to do and my areas of expertise. The
